79f502c379d086272fad76b6efb54a35a752bfcb
Rinad adel <studentrenad@gmail.com>
2026-07-11 23:51:48 +0300

    updat urls

--- happy_pet_backend/inventory/urls.py
+++ happy_pet_backend/inventory/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('import-product/', views.import_dropship_product, name='import_product'),
     path('products/', views.public_product_list, name='public_products'),
+    path('checkout/', views.process_checkout, name='checkout'),
 ]
