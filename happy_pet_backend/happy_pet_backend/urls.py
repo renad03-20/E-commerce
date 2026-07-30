baa9894b2d330ad72ccd2de1b8ad0e07c0e9eccd
Rinad adel <studentrenad@gmail.com>
2026-07-30 23:23:26 +0300

    fix url

--- happy_pet_backend/happy_pet_backend/urls.py
+++ happy_pet_backend/happy_pet_backend/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/admin/', include('inventory.urls')),
+    path('api/inventory/', include('inventory.urls')), 
 ]
