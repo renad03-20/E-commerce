d505c352284bf70f8a610cda188163ab622045ec
Rinad adel <studentrenad@gmail.com>
2026-07-31 22:13:26 +0300

    chore(backend): add env variables

--- happy_pet_backend/happy_pet_backend/settings.py
+++ happy_pet_backend/happy_pet_backend/settings.py
@@ -18,6 +18,8 @@ load_dotenv()
 
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 STRIPE_PUBLISHABLE_KEY = os.getenv("STRIPE_PUBLISHABLE_KEY")
+STRIPE_WEBHOOK_SECRET = os.getenv("STRIPE_WEBHOOK_SECRET") 
+ALIEXPRESS_API_KEY = os.getenv("ALIEXPRESS_API_KEY") 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
